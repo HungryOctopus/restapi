7a56f67062599fc354a78e317ad3b2edf26c8970
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2022-05-02 21:31:19 +0200

    Post model & routes

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv/config');
 const app = express();
 
+// Import routes
+
+const postsRoute = require('./routes/posts');
+
+// Middleware: use postsRoutes everytime you go to /posts
+app.use('/posts', postsRoute);
 
 //ROUTES
 
 app.get('/', (req, res, send) => {
-  res.send('Yeaaaaaah');
+  res.send('Home');
 });
 
-app.get('/yeah', (req, res, send) => {
-  res.send('Yeaaaaaah baby');
-});
+//Connect to DB
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log('connected to DB')
+);
 
 // How do we start listening to the server?
 app.listen(3000);
